f4876c6b40dba3372ce7fc271c3e4acb8215731c
Alexandre <alexandrewichmann2020@gmail.com>
2025-03-15 09:55:11 -0300

    Adiciona função 'atualizar_lista_amigos' para manter a lista atualizada

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ let amigos = [];
 
 function adicionarAmigo() {
     let entradaDoUsuario = document.getElementById("amigo").value;
+
     if (entradaDoUsuario.trim() !== "") {
         amigos.push(entradaDoUsuario);
         document.getElementById("amigo").value = "";
+        atualizarListaAmigos(amigos)
     } else {
         alert("Por favor, insira um nome.");
     }
