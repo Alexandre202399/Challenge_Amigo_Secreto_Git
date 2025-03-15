1876bf75442c404974b04c034fe51c1618b91aa0
Alexandre <alexandrewichmann2020@gmail.com>
2025-03-15 00:20:40 -0300

    Adiciona função 'adicionar_amigo' para gerenciar a inclusão de amigos

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let entradaDoUsuario = document.getElementById("amigo").value;
+    if (entradaDoUsuario.trim() !== "") {
+        amigos.push(entradaDoUsuario);
+        document.getElementById("amigo").value = "";
+    } else {
+        alert("Por favor, insira um nome.");
+    }
+}
